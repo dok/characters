81f8273c8026bb6ecaba745215343d7c597726a5
Sean Dokko <dokko1230@gmail.com>
2014-05-06 15:24:58 -0700

    moves keypress event handlers to the app model

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,36 @@
 var App = Backbone.Model.extend({
   initialize: function() {
     this.set('myPlayer', new Player('MARTH'));
+
+    myPlayer = this.get('myPlayer');
+
+    var socket = io.connect('http://localhost:3000');
+    this.set('socket', socket);
+
+    $('body').keypress(function(event) {
+      if(event.which === 119) { //w
+        myPlayer.set('top', myPlayer.get('top') - 5);
+      } else if(event.which === 97) { //a
+        myPlayer.set('left', myPlayer.get('left') - 5);
+      } else if(event.which === 115) { //s
+        myPlayer.set('top', myPlayer.get('top') + 5);
+      } else if(event.which === 100) { //d
+        myPlayer.set('left', myPlayer.get('left') + 5);
+      }
+      socket.emit('changePos',
+        {
+          top: myPlayer.get('top'),
+          left: myPlayer.get('left')
+        }
+      );
+    });
+
+
+    // socket.on('playerMove', function (data) {
+    //   console.log(data);
+    //   that.set('top', data.top);
+    //   that.set('left', data.left);
+    // });
   }
 });
 
@@ -9,33 +39,6 @@ var Player = Backbone.Model.extend({
     this.set('name', name);
     this.set('top', 85);
     this.set('left', 100);
-    var that = this;
-
-    $('body').keypress(function(event) {
-      if(event.which === 119) { //w
-        that.set('top', that.get('top') - 5);
-      } else if(event.which === 97) { //a
-        that.set('left', that.get('left') - 5);
-      } else if(event.which === 115) { //s
-        that.set('top', that.get('top') + 5);
-      } else if(event.which === 100) { //d
-        that.set('left', that.get('left') + 5);
-      }
-      socket.emit('changePos',
-        {
-          top: that.get('top'),
-          left: that.get('left')
-        }
-      );
-    });
-
-    var socket = io.connect('http://localhost:3000');
-    socket.on('playerMove', function (data) {
-      console.log(data);
-      that.set('top', data.top);
-      that.set('left', data.left);
-    });
-
   }
 });
 
